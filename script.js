302a5058563d5e0c6d8627c32588cbeb645dc946
Philip Sjøtlov <philipsjoetlov@gmail.com>
2022-09-09 10:58:10 +0200

    data virker nu

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const url = "https://database-4ee2.restdb.io/rest/skoudvalg";
 const header = document.querySelector("h2");
 const mereinfo = {
   headers: {
-    "x-apikey": "f19d5650d27a620e47987f0c68b8a6a4e5421",
+    "x-apikey": "631affccfdc15b0265f17258",
   },
 };
 
