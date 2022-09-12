469c931cc4f83aefffe4007e74721a755c7c9c73
fili5863​ <fili5863@stud.kea.dk>
2022-09-12 09:48:42 +0200

    java rettet

--- script.js
+++ script.js
@@ -60,10 +60,7 @@ function visDetaljer(sko) {
   popup.querySelector(".billedeurl").src = "skogalleri/" + sko.billedetekst + ".webp";
   popup.querySelector(".beskrivelse").textContent = sko.beskrivelse;
   popup.querySelector(".pris").textContent = sko.pris + "kr.";
+  popup.addEventListener("click", () => (popup.style.display = "none"));
 }
-document.querySelector("#popupKnap").addEventListener("click", forsvindKnap);
 
-function forsvindKnap() {
-  popup.style.display = "none";
-}
 hentData();
